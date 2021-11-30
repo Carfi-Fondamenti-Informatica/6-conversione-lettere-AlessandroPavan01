f1859e8b64aff64b5144712b7980fddb4de11e22
AlessandroPavan01 <91598666+AlessandroPavan01@users.noreply.github.com>
2021-11-30 21:43:32 +0100

    Update lib.h

--- lib.h
+++ lib.h
@@ -1,1 +1,9 @@
+//
+// Created by Giorgio on 30/11/2021.
+//
 
+#ifndef ESERCITAZIONE6_LIBRERIA_H
+#define ESERCITAZIONE6_LIBRERIA_H
+
+#endif //ESERCITAZIONE6_LIBRERIA_H
+bool conversione ( char & );
